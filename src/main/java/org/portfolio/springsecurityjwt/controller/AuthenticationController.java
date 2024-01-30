0d1e012a5c7996ac32ba4f7069f15b3c7b57f378
allamymp <allamympereira@gmail.com>
2024-01-30 00:41:18 -0300

    cookies implemented.

--- src/main/java/org/portfolio/springsecurityjwt/controller/AuthenticationController.java
+++ src/main/java/org/portfolio/springsecurityjwt/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package org.portfolio.springsecurityjwt.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.portfolio.springsecurityjwt.security.AuthenticationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,13 @@ public class AuthenticationController {
     @PostMapping("authenticate")
     public String authenticate(Authentication authentication) {
         return authenticationService.authenticate(authentication);
+    }
+    @PostMapping("authenticate-cookie")
+    public ResponseEntity<?> authenticateCookie(Authentication authentication, HttpServletResponse response){
+        Cookie cookie = new Cookie("token",authenticationService.authenticate(authentication));
+        cookie.setMaxAge(3600);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/portfolio/springsecurityjwt/controller/ClientController.java
+++ src/main/java/org/portfolio/springsecurityjwt/controller/ClientController.java
@@ -1,15 +1,19 @@
 package org.portfolio.springsecurityjwt.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.portfolio.springsecurityjwt.model.entities.User;
 import org.portfolio.springsecurityjwt.model.enums.Role;
 import org.portfolio.springsecurityjwt.model.record.UserCreateRecord;
 import org.portfolio.springsecurityjwt.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -18,7 +22,7 @@ public class ClientController {
 
     private final UserRepository repository;
 
-   private final PasswordEncoder encoder;
+    private final PasswordEncoder encoder;
 
     public ClientController(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
@@ -41,6 +45,26 @@ public class ClientController {
         Optional<User> user = repository.findByUsername(username);
         if (user.isPresent()) {
             return ResponseEntity.ok().body(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping("{username}-cookie")
+    public ResponseEntity<?> getUserByUsernameCookie(@PathVariable @Validated String username, HttpServletResponse response) {
+        Optional<User> user = repository.findByUsername(username);
+        if (user.isPresent()) {
+            Cookie cookie = new Cookie("user",
+                    user.get().getId() + "-"
+                            + user.get().getUsername() + "-"
+                            + user.get().getEmail() + "-"
+                            + user.get().getPassword() + "-"
+                            + user.get().getRole()
+            );
+            cookie.setMaxAge(3600);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
